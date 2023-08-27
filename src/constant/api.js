b02a8394ed97ab2c4df7bb47befa534268f434ab
Ajay kumar sharma <48996332+creativeweb-aj@users.noreply.github.com>
2023-08-27 17:20:16 +0530

    update api.js

--- src/constant/api.js
+++ src/constant/api.js
@@ -3,7 +3,7 @@ export const API = {
       process.env.NODE_ENV === "development"
         ? "http://192.168.1.7:8010/api/v1"
         : "https://mahalaxmi-crane-service.onrender.com/api/v1",
-    Baser_Url: 
+    Base_Url: 
       process.env.NODE_ENV === "development"
         ? "http://192.168.1.7:8010"
         : "https://mahalaxmi-crane-service.onrender.com",
