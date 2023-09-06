15896653431ef35247bf24a13d7e4a8d79c921f4
Ajay kumar sharma <48996332+creativeweb-aj@users.noreply.github.com>
2023-09-06 15:37:24 +0530

    update const

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,10 +1,10 @@
 export const API = {
     PORT:
       process.env.NODE_ENV === "development"
-        ? "https://mcsadmin.pythonanywhere.com/api/v1"
+        ? "http://0.0.0.0:8010/api/v1"
         : "https://mcsadmin.pythonanywhere.com/api/v1",
     Base_Url: 
       process.env.NODE_ENV === "development"
-        ? "https://mcsadmin.pythonanywhere.com/"
+        ? "http://0.0.0.0:8010/"
         : "https://mcsadmin.pythonanywhere.com/",
 };
